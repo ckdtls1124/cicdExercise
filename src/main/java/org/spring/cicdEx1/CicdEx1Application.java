c1eea80db5707415d78806d409735d425ef29f59
ckdtls1124 <ckdtls1124@naver.com>
2023-03-02 10:42:32 +0900

    first_cicdEx1

---
+++ src/main/java/org/spring/cicdEx1/CicdEx1Application.java
@@ -0,0 +1,13 @@
+package org.spring.cicdEx1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdEx1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdEx1Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/spring/cicdEx1/CicdEx1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring.cicdEx1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdEx1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
